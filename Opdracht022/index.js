12b0acf1be1e93bdafbd1af135fb233d43109edd
RichardWillems54 <76516026+RichardWillems54@users.noreply.github.com>
2021-04-20 14:46:45 +0200

    Add files via upload

---
+++ Opdracht022/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy")
+let name = "Richard Willems"
+console.log(name);
+let a = 6;
+let b = 7;
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
+let age = "48";
+console.log(typeof age);
