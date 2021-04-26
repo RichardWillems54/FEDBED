06ada14f1b6706db2a17dfe4a7492a856a310c6e
RichardWillems54 <76516026+RichardWillems54@users.noreply.github.com>
2021-04-26 12:00:49 +0200

    Add files via upload

---
+++ Opdracht024/index.js
@@ -0,0 +1,15 @@
+let age = 19;
+let Fem = true;
+var Driverstat = "Bob";
+
+if (age >= 18 && Fem) {
+    console.log("Je mag naar binnen.")
+}
+else {
+    console.log("Je mag niet naar binnen.")
+}
+if (Driverstat == "Bob") {
+    console.log("Je mag rijden")
+} else {
+    console.log("Je mag niet rijden")
+}
