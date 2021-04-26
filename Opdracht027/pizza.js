b7e328c75eab1c65cc2130535f3b899b2de4ad50
RichardWillems54 <76516026+RichardWillems54@users.noreply.github.com>
2021-04-26 21:08:25 +0200

    Add files via upload

---
+++ Opdracht027/pizza.js
@@ -0,0 +1,23 @@
+const pizza = function () {
+    console.log("Deeg uitrollen");
+    console.log("Tomatensaus uitsmeren");
+    console.log("Salami erop leggen");
+    console.log("Paprika erop leggen");
+    console.log("Kaas erop strooien");
+    console.log("Pizza bakken");
+}
+
+const sushi = function () {
+    console.log("Cook rice");
+    console.log("Prepare fish")
+    console.log("Prepare sushi")
+}
+sushi();
+sushi();
+sushi();
+
+pizza();
+pizza();
+
+
+
