98281193df4abcf02658a936d82253874bb5ad86
RichardWillems54 <76516026+RichardWillems54@users.noreply.github.com>
2021-04-27 11:58:42 +0200

    Add files via upload

---
+++ Opdracht028/Paint.js
@@ -0,0 +1,7 @@
+const paint = function(kleur1, kleur2){
+    console.log("The north wall has been painted " + kleur1 + ".")
+    console.log("The south-east wall has been painted " + kleur2 + ".")
+}
+const color1 = "grey";
+const color2 = "orange";
+paint(color1, color2);
